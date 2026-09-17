7679c8df7a492dc3986311a38224430db1c2d511
arvaro123 <alvarorogerio12340@gmail.com>
2026-09-17 12:37:43 -0300

    Update script1.js
    melhoramento na sintaxe do codigo

--- script1.js
+++ script1.js
@@ -1,7 +1,18 @@
-let numeroAleatorio
-let tentativas
-let mensagem
+function startGame() {
+    alert("Bem vindo, vamos começar o nosso desafio de adivinhação");
+    const num1 = Math.floor(Math.random() * 100) + 1;
+    const respostaCerta = num1;
 
-function blabla(){
-    
+    let contador = 0;
+    let palpite = prompt("Qual o número que eu escolhi?");
+   
+    while (palpite != respostaCerta) {
+        if (palpite > respostaCerta) {
+            alert("Muito alto");
+        } else if (palpite < respostaCerta) {
+            alert("Muito baixo!");
+        }
+     contador++;
+     palpite = prompt("Qual o número que eu escolhi?");
 }
+    alert("Você adivinhou! Com um total de: " + contador + " tentativas!");
